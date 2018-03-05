7f87d37d0f8f7c219c1e6da4a475aff1f3591ff1
holmeshun <holmeshun@gmail.com>
2018-03-05 16:52:18 +0100

    Add files via upload

--- Szakdoga0.2/src/sample/Controller.java
+++ Szakdoga0.2/src/sample/Controller.java
@@ -1,22 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.net.MalformedURLException;
 
@@ -35,55 +33,70 @@ public class Controller {
     Rectangle rectangle;
     @FXML
     Slider zoomslider;
+    @FXML
+    Canvas drawCanvas;
 
 
 
     // Egyéb változók ------------------------------------
-    FileChooser fileChooser = new FileChooser();
-    FileChooser.ExtensionFilter extensionFilterPNG = new FileChooser.ExtensionFilter(".png","*.png");
-    FileChooser.ExtensionFilter extensionFilterJPG = new FileChooser.ExtensionFilter(".jpg","*.jpg");
-    FileChooser.ExtensionFilter extensionFilterJPEG = new FileChooser.ExtensionFilter(".jpeg","*.jpeg");
+    private FileChooser fileChooser = new FileChooser();
 
-    double orgSceneX, orgSceneY;
-    double orgTranslateX, orgTranslateY;
-    double rectorgTranslateX, rectorgTranslateY;
+    private double orgSceneX, orgSceneY;
+    private double orgTranslateX, orgTranslateY;
+    private double rectorgTranslateX, rectorgTranslateY;
 
-    double smallScaleX, smallScaleY;
+    private double smallScaleX, smallScaleY;
+
+    private final static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private final static double screenWidth = screenSize.getWidth();
+    private final static double screenHeight = screenSize.getHeight();
+
+    private boolean isDrawMode = false;
+    final GraphicsContext graphicsContext = drawCanvas.getGraphicsContext2D();
+
+    private final static File initialFile = new File("src");
 
     public void initialize() {
+        FileChooser.ExtensionFilter extensionFilterPNG = makeFilter("png");
+        FileChooser.ExtensionFilter extensionFilterJPG = makeFilter("jpg");
+        FileChooser.ExtensionFilter extensionFilterJPEG =  makeFilter("jpeg");
 
         rectangle.setFill(Color.TRANSPARENT);
         rectangle.setStroke(Color.RED);
         rectangle.setVisible(false);
+
         fileChooser.getExtensionFilters().addAll(extensionFilterJPG, extensionFilterPNG,extensionFilterJPEG);
         fileChooser.setSelectedExtensionFilter(extensionFilterJPEG);
         fileChooser.setTitle("Choose your file");
-        File initialFile = new File("src");
         fileChooser.setInitialDirectory(initialFile);
+
+
     }
 
     public void handleOpenfilebtn(){
         File pictureFile = fileChooser.showOpenDialog(openfilebtn.getScene().getWindow());
+
         try {
             Image pictureImg = new Image(pictureFile.toURI().toURL().toString());
             mainimgview.setImage(pictureImg);
             smallimgview.setImage(pictureImg);
-            mainimgview.setFitWidth(1920);
-            mainimgview.setPreserveRatio(true);
-            mainimgview.toBack();
-            StackPane.setAlignment(mainimgview, Pos.CENTER);
-            rectangle.setVisible(true);
-            smallimgview.setFitHeight(mainimgview.getImage().getHeight()/10);
-            smallimgview.setFitWidth(mainimgview.getImage().getWidth()/10);
-            rectangle.setHeight(smallimgview.getFitHeight());
-            rectangle.setWidth(smallimgview.getFitWidth());
-            smallScaleX = smallimgview.getFitWidth()/mainimgview.getFitWidth();
-            smallScaleY = smallimgview.getFitHeight()/(mainimgview.getFitWidth()/mainimgview.getImage().getWidth()*mainimgview.getImage().getHeight());
-
         }
+
         catch (MalformedURLException ex){
             System.err.println("Malformed URL Exception!");
         }
+
+        mainimgview.setFitWidth(screenWidth);
+        setFitHeight(mainimgview);
+
+        smallimgview.setFitHeight(mainimgview.getImage().getHeight()/10);
+        smallimgview.setFitWidth(mainimgview.getImage().getWidth()/10);
+        smallScaleX = smallimgview.getFitWidth()/mainimgview.getFitWidth();
+        smallScaleY = smallimgview.getFitHeight()/mainimgview.getFitHeight();
+
+        rectangle.setVisible(true);
+        rectangle.setHeight(smallimgview.getFitHeight());
+        rectangle.setWidth(smallimgview.getFitWidth());
     }
 
     public void handleMouseClick(MouseEvent event){
@@ -94,8 +107,58 @@ public class Controller {
         rectorgTranslateX = rectangle.getTranslateX();
         rectorgTranslateY = rectangle.getTranslateY();
 
+        if(isDrawMode){
+            initDraw(graphicsContext);
+            graphicsContext.beginPath();
+            graphicsContext.moveTo(event.getX(), event.getY());
+            graphicsContext.stroke();
+        }
+    }
+    public void handleMouseReleased(MouseEvent event)
+    {
+
     }
 
+    public void handleDrawMode()
+    {
+       if(isDrawMode) {
+            isDrawMode = false;
+        }else{
+            isDrawMode = true;
+        }
+    }
+    /*public GraphicsContext getGraphicsContext()
+    {
+        GraphicsContext graphicsContext = drawCanvas.getGraphicsContext2D();
+        try{
+              graphicsContext = drawCanvas.getGraphicsContext2D();
+            return graphicsContext;
+        }catch(NullPointerException e){
+            System.out.println(e.toString());
+        }
+        return  graphicsContext;
+        //todo ide kéne egy return graphicsContext; de ezt nem engedi és emiatt hiba van
+    }*/
+    private void initDraw(GraphicsContext gc){
+        double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(5);
+
+        gc.fill();
+        gc.strokeRect(
+                0,              //x of the upper left corner
+                0,              //y of the upper left corner
+                canvasWidth,    //width of the rectangle
+                canvasHeight);  //height of the rectangle
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
+
+    }
     public void handleMouseDrag(MouseEvent event){
         double offsetX = event.getSceneX() - orgSceneX;
         double offsetY = event.getSceneY() - orgSceneY;
@@ -104,9 +167,7 @@ public class Controller {
         double rectnewTranslateX = rectorgTranslateX - offsetX*smallScaleX*(1/mainimgview.getScaleX());
         double rectnewTranslateY = rectorgTranslateY - offsetY*smallScaleY*(1/mainimgview.getScaleX());
 
-        if (rectangle.getWidth()<smallimgview.getFitWidth() &&
-            960-(mainimgview.getImage().getWidth()/2*mainimgview.getScaleX()-Math.abs(newTranslateX))<=0 &&
-            540-(mainimgview.getImage().getHeight()/2*mainimgview.getScaleY()-Math.abs(newTranslateY))<=0){
+        if (!isFullSize(smallimgview,rectangle) && isInFocus(mainimgview,newTranslateX,newTranslateY)){
 
                 mainimgview.setTranslateX(newTranslateX);
                 mainimgview.setTranslateY(newTranslateY);
@@ -114,28 +175,45 @@ public class Controller {
                 rectangle.setTranslateY(rectnewTranslateY);
         }
 
+        if(isDrawMode){
+            graphicsContext.lineTo(event.getX(), event.getY());
+            graphicsContext.stroke();
+        }
+
     }
 
     public void handleScroll(ScrollEvent e){
-        if (e.getDeltaY()>0 && rectangle.getWidth() > 10) {
-            mainimgview.setScaleX(mainimgview.getScaleX()*1.25);
-            mainimgview.setScaleY(mainimgview.getScaleY()*1.25);
-            rectangle.setWidth(rectangle.getWidth()*0.8);
-            rectangle.setHeight(rectangle.getHeight()*0.8);
+
+        double scrollValue = e.getDeltaY();
+
+        double oldtranslateX = mainimgview.getTranslateX();
+        double oldtranslateY = mainimgview.getTranslateY();
+
+        double mainimgScaleUpValue = 1.25;
+        double mainimgScaleDownValue = 0.8;
+        double rectScaleDownValue = 1/mainimgScaleUpValue;
+        double rectScaleUpValue = 1/mainimgScaleDownValue;
+
+        if (scrollValue>0 && rectangle.getWidth() > 10) {
+            goToOrgPos(mainimgview);
+            doScale(mainimgview,mainimgScaleUpValue);
+            doScale(rectangle,rectScaleDownValue);
+            moveObj(mainimgview,oldtranslateX*mainimgScaleUpValue,oldtranslateY*mainimgScaleUpValue);
             zoomslider.setValue(mainimgview.getScaleX());
+
+
         }
-        else if(1.25*rectangle.getWidth() <= smallimgview.getFitWidth()){
-            mainimgview.setScaleX(mainimgview.getScaleX()*0.8);
-            mainimgview.setScaleY(mainimgview.getScaleY()*0.8);
-            rectangle.setWidth(rectangle.getWidth()*1.25);
-            rectangle.setHeight(rectangle.getHeight()*1.25);
+        else if(mainimgScaleUpValue*rectangle.getWidth() <= smallimgview.getFitWidth()){
+            goToOrgPos(mainimgview);
+            doScale(mainimgview,mainimgScaleDownValue);
+            doScale(rectangle,rectScaleUpValue);
+            moveObj(mainimgview,oldtranslateX*mainimgScaleDownValue,oldtranslateY*mainimgScaleDownValue);
             zoomslider.setValue(mainimgview.getScaleX());
-        }
+            }
+
         else {
-            mainimgview.setTranslateX(0);
-            mainimgview.setTranslateY(0);
-            rectangle.setTranslateX(0);
-            rectangle.setTranslateY(0);
+            goToOrgPos(mainimgview);
+            goToOrgPos(rectangle);
             mainimgview.setScaleX(1);
             mainimgview.setScaleY(1);
             rectangle.setWidth(smallimgview.getFitWidth());
@@ -146,15 +224,82 @@ public class Controller {
     }
 
     public void handleSlider(){
+        double rectNewWidth = smallimgview.getFitWidth()*(1/zoomslider.getValue());
+        double rectNewHeight = smallimgview.getFitHeight()*(1/zoomslider.getValue());
+
+
         mainimgview.setScaleX(zoomslider.getValue());
         mainimgview.setScaleY(zoomslider.getValue());
-        rectangle.setWidth(1/zoomslider.getValue());
-        rectangle.setHeight(1/zoomslider.getValue());
+        rectangle.setWidth(rectNewWidth);
+        rectangle.setHeight(rectNewHeight);
     }
 
     public void handleExitbtn(){
         Stage stage = (Stage) exitbtn.getScene().getWindow();
         stage.close();
     }
+
+    private static void doScale(ImageView imgview, double scale){
+        //az adott objektumot méretarányt tartva adott scale-el növel
+        imgview.setScaleX(imgview.getScaleX()*scale);
+        imgview.setScaleY(imgview.getScaleY()*scale);
+    }
+
+    private static void doScale(Rectangle rect, double scale){
+        //mint az imageviewnél (itt nem a scale változót növeljük)
+        rect.setWidth(rect.getWidth()*scale);
+        rect.setHeight(rect.getHeight()*scale);
+    }
+
+    private void goToOrgPos(ImageView imgview){
+        //visszateszi az adott objektumot az eredeti pozíciójába
+        imgview.setTranslateX(0);
+        imgview.setTranslateY(0);
+    }
+
+    private void goToOrgPos(Rectangle rect){
+        //mint az imageview esetén
+        rect.setTranslateX(0);
+        rect.setTranslateY(0);
+    }
+
+    private void moveObj(ImageView imgview, double posX,double posY){
+        //adott objektumot adott pozícióba helyez
+        imgview.setTranslateX(posX);
+        imgview.setTranslateY(posY);
+    }
+
+    private void setFitHeight(ImageView imgview){
+        //szükséges mert csak a fitWidth változót állítjuk be és a preserve ratio adja meg a height-ot azonban nem tárolja el a fitHeight
+        imgview.setFitHeight ((imgview.getFitWidth()*imgview.getImage().getHeight())/imgview.getImage().getWidth());
+    }
+
+    private boolean isInFocus(ImageView imgview,double newTranslateX,double newTranslateY){
+        //a megnyitott kép széleinek távolságát méri aképernyő széleitől, true ha nincs "fehér csík"
+        double distanceX = 960-(((imgview.getFitWidth()/2)*imgview.getScaleX())-Math.abs(newTranslateX));
+        double distanceY = 540-(((imgview.getFitHeight()/2)*imgview.getScaleY())-Math.abs(newTranslateY));
+
+        if (distanceX>0){
+            return false;
+        }
+        else if (distanceY>0){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isFullSize(ImageView imgview,Rectangle rect){
+        //ezzel állapítom meg hogy a kép teljesen ki van e nagyítva.. lehet mainimgview.scalex==1-el egyszerűbb lenne
+        if (rect.getWidth()<imgview.getFitWidth()){
+            return false;
+        }
+        return true;
+    }
+
+    private FileChooser.ExtensionFilter makeFilter(String extension){
+        return new FileChooser.ExtensionFilter("." + extension,"*." + extension);
+    }
+
+
 
 }
